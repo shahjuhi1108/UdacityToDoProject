e2be31cab7a743556393d088ceaf0e86b98cd924
Juhi <shahjuhi1108@gmail.com>
2020-09-23 01:00:09 -0700

    Segregation of code

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,12 +1,9 @@
 import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
-//import * as AWS from 'aws-sdk'
 import { createLogger } from '../../utils/logger'
 import { getUserId } from '../utils'
 import { getTodosPerUser } from '../../businessLogic/todos'
 
-// const docClient = new AWS.DynamoDB.DocumentClient()
-//const todoTable = process.env.TODO_TABLE
 const logger = createLogger('todos')
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
@@ -15,7 +12,7 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
 
   const userId = getUserId(event)
 
-  logger.info(`get groups for user ${userId}`)
+  logger.info(`get todos for user ${userId}`)
 
   const todos = await getTodosPerUser(userId)
 
@@ -29,16 +26,3 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
     })
   }
 }
-
-// async function getTodosPerUser(userId: string) {
-//   const result = await docClient.query({
-//     TableName: todoTable,
-//     KeyConditionExpression: 'userId = :userId',
-//     ExpressionAttributeValues: {
-//       ':userId': userId,
-//     },
-//     ScanIndexForward: false
-//   }).promise()
-
-//   return result.Items
-// }
